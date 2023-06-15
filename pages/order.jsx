006aaa649082b01ac4ac5954ecc8b9b72452a306
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-06-15 14:35:18 +0530

    Dashboard

---
+++ pages/order.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {FaShopping } from 'react-icons/fa'
+import { BsThreeDots } from 'react-icons/bs'
+import {data} from '../data/data'
+
+
+const order = () => {
+  return (
+    <div>order</div>
+  )
+}
+
+export default order
